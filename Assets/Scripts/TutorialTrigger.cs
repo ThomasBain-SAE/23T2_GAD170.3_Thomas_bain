cc80ca579aea7e65c84086524d0e76e8a2a98085
ThomasBain-SAE <136556449+ThomasBain-SAE@users.noreply.github.com>
2023-08-14 19:46:17 +1000

    changes

--- Assets/Scripts/TutorialTrigger.cs
+++ Assets/Scripts/TutorialTrigger.cs
@@ -1,26 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 
-
-//use instantiate 
-//use addforce for direction
 public class TutorialTrigger : MonoBehaviour
 {
-    //When the player character enters this zone
-    //display the toutorial text
-    [SerializeField] TextMeshPro tutorialText;
+    public GameObject tutorialText; // Assign the TextMeshPro GameObject in the Inspector
+
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Ouch, !I have Triggered something.");
-
-        //If the player character triggers this method, do the thing
         if (other.CompareTag("Player"))
-
         {
-            //Show the tutorial text
-            tutorialText.enabled = true;
+            Debug.Log("Player has entered the trigger zone.");
+
+            // Show the tutorial text
+            tutorialText.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player has exited the trigger zone.");
+
+            // Hide the tutorial text
+            tutorialText.SetActive(false);
         }
     }
 }
